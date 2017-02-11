a4c50f93b00d0a6ddd1399208eca4a07cefc94fd
Jacob Lever <jacobianism@gmail.com>
2017-02-11 00:01:06 +0000

    Add a quick (-q) option to run.sh (this does not run ensureSetup.py)

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
-python3 manage.py shell < transactions/src/ensureSetup.py
+if  [[ $1 != "-q" ]]; then
+    python3 manage.py shell < transactions/src/ensureSetup.py
+fi
 python3 manage.py runserver
 
