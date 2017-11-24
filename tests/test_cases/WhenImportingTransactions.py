ad8634c8c9f2b21d3cfad6abfaa7d0f6a7620396
Jacob Lever <jacobianism@gmail.com>
2017-11-24 00:57:57 +0000

    Remove sample tests

--- tests/test_cases/WhenImportingTransactions.py
+++ tests/test_cases/WhenImportingTransactions.py
@@ -62,14 +62,3 @@ class WhenImportingTransactions(DatabaseBackedFinanceTestCase):
         self.assertEqual(
             property(self.import_file.transactions[0]),
             property(Transaction.objects.all()[0]))
-
-        # def test_isupper(self):
-        #     self.assertTrue('FOO'.isupper())
-        #     self.assertFalse('Foo'.isupper())
-        #
-        # def test_split(self):
-        #     s = 'hello world'
-        #     self.assertEqual(s.split(), ['hello', 'world'])
-        #     # check that s.split fails when the separator is not a string
-        #     with self.assertRaises(TypeError):
-        #         s.split(2)
